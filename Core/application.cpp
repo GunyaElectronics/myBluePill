5f5cd664c5ebeff73d706fd0e4354f455b13b67c
maxim.hunko <maxim.hunko@developex.net>
2022-06-01 14:42:24 +0300

    Change receive method to cmsis os queue.

--- Core/application.cpp
+++ Core/application.cpp
@@ -1,27 +1,20 @@
 #include "cmsis_os.h"
 #include "BSP.h"
 #include "application.h"
+#include "asserts.h"
 
-uint32_t gRxCount = 0;
-uint32_t gTxCount = 0;
-uint8_t charBuffer[2];
+static const uint32_t kMaxAppTimeoutTicks = UINT32_MAX;
+static osMessageQueueId_t gConsoleRxQueue = NULL;;
 
 void BSP_consoleReceivedByteCb(uint8_t byte)
 {
-    static uint8_t index = 0;
-#if 0
-    putToQueueFromIsr(byte);
-#endif
-    charBuffer[index] = byte;
-    gRxCount++;
-    index = index ? 0 : 1;
+    if (gConsoleRxQueue != NULL) {
+        osMessageQueuePut(gConsoleRxQueue, &byte, 0U, 0U);
+    }
 }
 
 void application(void)
 {
-    static uint8_t index = 0;
-    uint8_t receivedByte = 0;
-
     const BSP_uartNumber_t consoleUartNumber = 1;
     const BSP_uartHandle_t consoleConfig = {
         .number           = consoleUartNumber,
@@ -30,25 +23,23 @@ void application(void)
         .receivedByteCb   = BSP_consoleReceivedByteCb,
     };
 
-    BSP_uartInit(&consoleConfig, 115200);
+    const uint8_t kQueueElementCount = 2U;
+    uint8_t receivedByte = 0U;
+    gConsoleRxQueue = osMessageQueueNew(kQueueElementCount, sizeof(receivedByte), NULL);
+
+    ASSERT(gConsoleRxQueue != NULL);
+
+    BSP_uartInit(&consoleConfig, 115200U);
     BSP_uartStartReceive(consoleUartNumber);
 
     while (true) {
-#if 0
-        if (getFromQueue(&receivedByte))
-#endif
-        if (gTxCount != gRxCount) {
-            receivedByte = charBuffer[index];
-            index = index ? 0 : 1;
-            gTxCount++;
+        if (osMessageQueueGet(gConsoleRxQueue, &receivedByte, NULL, kMaxAppTimeoutTicks) == osOK) {
             BSP_greenLedToggle();
             if (receivedByte == '\r') {
                 BSP_uartSendBlocking(consoleUartNumber,(uint8_t *) "\r\n", sizeof("\r\n"));
             } else {
                 BSP_uartSendBlocking(consoleUartNumber, &receivedByte, sizeof(receivedByte));
             }
-        } else {
-            osDelay(2);
         }
     }
 }
