d5323fbb73ac2c294f3c37051a453b15ff70d14e
MaximHunko <maxim.hunko@developex.net>
2023-05-18 16:49:38 +0300

    Refactored CommandConsole class.

--- Core/application.cpp
+++ Core/application.cpp
@@ -7,41 +7,20 @@
 #include "commandConsole.h"
 #include "asserts.h"
 #include "utils.h"
-#include <stdio.h>
-
-static bool helpCmd(const char **pParams, size_t paramCount);
-static bool exitCmd(const char **pParams, size_t paramCount);
-static bool rebootCmd(const char **pParams, size_t paramCount);
-
-static const Command allCmds[] = {
-    { "?",      "[command]",          "Show help", helpCmd },
-    { "help",   "[command]",          "Show help", helpCmd },
-    { "exit",   "",                   "Exit from the console", exitCmd },
-    { "reboot", "[-d] [delay value]", "Example: reboot -d 2 "
-                                      "(delay 2 seconds before reboot)", rebootCmd },
-};
-
-static ISerialInputOutput *pIo = NULL;
-static CommandConsole *pConsole = NULL;
 
 void application(void)
 {
-    CommandConsole console = { allCmds, COUNT_OF(allCmds) };
-    pConsole = &console;
-    const uint32_t kBlinkPeriodMs = 500;
+    SerialPortIo   uartPio = { 1, 115200 };
+    CommandConsole console = { uartPio };
+
+    SPI loraSpi = 0;
+    loraSpi.nssHigh();
+
     GPIO led = PIN_PC13;
     led.modeOutput();
 
-    SerialPortIo uartPio = {1, 115200};
-
-    SPI loraSpi = 0;
-
-    loraSpi.nssHigh();
-
-    pIo = &uartPio;
-    console.start(pIo);
-
     while (true) {
+        const uint32_t kBlinkPeriodMs = 500;
         led = true;
         osWrapper::Task::delay(kBlinkPeriodMs);
         led = false;
@@ -49,51 +28,15 @@ void application(void)
     }
 }
 
-static bool helpCmd(const char **pParams, size_t paramCount)
+bool CommandConsole::rebootCmd()
 {
-    static char helpString[128];
-
-    if (paramCount) {
-        // if selected one command - view detail info.
-        const Command *pCmd = pConsole->findCommand(pParams[0]);
-        if (pCmd != NULL) {
-            snprintf(helpString, sizeof(helpString),
-                     " Syntax:\r\n"
-                     "\t%s\t%s\r\n"
-                     " Description:\r\n"
-                     "\t%s\r\n",
-                     pCmd->pCmd,
-                     pCmd->pHelp,
-                     pCmd->pReference);
-            pIo->putString(helpString);
-        } else {
-            pIo->putString((char *)" Command not found\r\n");
-        }
+    if (handlerParamsCount) {
+        io.putString((char *)" System will be rebooted after ");
+        io.putString((char *)pParams[0]);
+        io.putString((char *)" second\r\n");
     } else {
-        for (size_t i = 0; i < COUNT_OF(allCmds); i++) {
-            snprintf(helpString, sizeof(helpString), "\t%s\t%s\r\n",
-                     allCmds[i].pCmd,
-                     allCmds[i].pHelp);
-            pIo->putString(helpString);
-        }
+        io.putString((char *)" Rebooting...\r\n");
     }
 
-    return false;
-}
-
-static bool exitCmd(const char **pParams, size_t paramCount)
-{
-    return true;
-}
-
-static bool rebootCmd(const char **pParams, size_t paramCount)
-{
-    if (paramCount) {
-        pIo->putString((char *)" System will be reboted after ");
-        pIo->putString((char *)pParams[1]);
-        pIo->putString((char *)" second\r\n");
-    } else {
-        pIo->putString((char *)" Rebooting...\r\n");
-    }
     return false;
 }
